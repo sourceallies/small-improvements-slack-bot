0372cb7e85368bac0660d9904e20b7e4fe1dcae8
Reece Appling <rappling@Sources-MBP-4.lan>
2022-06-06 18:58:21 -0400

    Last for today, sorted variables

--- variable.tf
+++ variable.tf
@@ -1,4 +1,7 @@
-variable "region" {}
+# vars prepended with TV_VAR_
 variable "AWS_ACCESS_KEY_ID" {}
 variable "AWS_SECRET_ACCESS_KEY" {}
+
+# Vars with normal access types
+variable "region" {}
 variable "organization" {}
