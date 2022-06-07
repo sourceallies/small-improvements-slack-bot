4501597e297eaa4ac9e17a753513d304da04eff0
Reece Appling <rappling@Sources-MBP-4.lan>
2022-06-07 11:27:36 -0400

    clean

--- dev.tfvars
+++
@@ -1,2 +0,0 @@
-region="us-east-1"
-organization="SAI"
--- variable.tf
+++
@@ -1,7 +0,0 @@
-# vars prepended with TV_VAR_
-variable "AWS_ACCESS_KEY_ID" {}
-variable "AWS_SECRET_ACCESS_KEY" {}
-
-# Vars with normal access types
-variable "region" {}
-variable "organization" {}
