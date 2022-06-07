75a180d4bb9e51df3b3a47f27decfe3a68ea921f
Reece Appling <rappling@Sources-MBP-4.lan>
2022-06-07 10:36:04 -0400

    Axios and AWS SDK

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const axios = require('axios');//Axios for http requests
+const AWS = require('aws-sdk');//AWS SDK for DynamoDB
+const docClient = new AWS.DynamoDB.DocumentClient();
 // Put lambda function in here :)
 
 'use strict';
@@ -11,7 +14,12 @@ exports.handler = (event, context, callback) => {
 };
 
 function getObjectives(earliest){
-
+    axios.get('https://example.com/todos').then(res => {
+        console.log(`statusCode: ${res.status}`);
+        console.log(res);
+    }).catch(error => {
+        console.error(error);
+    });
 }
 
 function workWithDatabase(){
@@ -23,3 +31,6 @@ function postToSlack(posts){//posts are an array
 }
 
 // schedule with cloudwatch rule -> cron(0 */12 * * *);
+
+//For DynamoDB, reference the following:
+// https://docs.amplify.aws/guides/functions/dynamodb-from-js-lambda/q/platform/js/#scanning-a-table
