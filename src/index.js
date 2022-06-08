582f89ad06975ff149a61936910cc0ddef469706
Reece Appling <rappling@Sources-MBP-4.lan>
2022-06-08 13:23:56 -0400

    remove https

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const https = require('https');
+const { rawListeners } = require('process');
 // Load the AWS SDK
 var AWS = require('aws-sdk'),
     region = "us-east-1",
@@ -18,9 +19,7 @@ var client = new AWS.SecretsManager({
 function getSecret(secretName){
     return new Promise((accept,reject)=>{
         client.getSecretValue({SecretId: secretName}, function(err, data) {
-            if(err){
-                reject(`Could not get Secret: ${err.code}`);
-            }
+            if(err){reject(`Could not get Secret: ${err.code}`);}
             else{
                 // Decrypts secret using the associated KMS key.
                 // Depending on whether the secret is a string or binary, one of these fields will be populated.
@@ -38,7 +37,7 @@ function getSecret(secretName){
 
 
 var httpsOptions = {
-    hostname: 'https://allies.small-improvements.com',
+    hostname: 'allies.small-improvements.com',
     port: 443,
     path: '/api/v2/activities?modules=OBJECTIVE',
     method: 'GET',
@@ -62,18 +61,22 @@ function getObjectives(opts){
     return new Promise((accept,reject)=>{
         const req = https.request(opts, res => {
             var toReturn = "";
-            if(res.statusCode != 200&&res.statusCode != 307){
+            if(res.statusCode != 200){
+                console.log(`status logged ${res.statusCode}`);
                 reject(`Could not get objectives: ${res.statusCode}`);
+                return;
             }
             res.on('data', d => { //Concat new string onto old string, is this necessary? Can data be paginated?
                 toReturn += d;
             });
             res.on('close',()=>{
+                console.log('RETRIEVED===============================');
+                console.log(toReturn);
                 accept(JSON.parse(toReturn));
             });
         });
-        req.on('error', error => {
-            reject(`https error: ${error}`);
+        req.on('error', err => {
+            reject(`https error: ${err}`);
         });
         req.write("data");
         req.end();
@@ -99,7 +102,10 @@ async function main(event,context,callback){
     try{
         let secrets = await getSecret(secretName);
         let SIToken = secrets.SIToken;
-        httpsOptions.headers = {Authorization: `Bearer ${SIToken}`};
+        httpsOptions.headers = {
+            Authorization: `Bearer ${SIToken}`,
+            Accept: `application/json`
+        };
         let slackToken = secrets.SlackToken;
         let objectives = await getObjectives(httpsOptions);
         tryDB = true;
