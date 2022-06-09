483eb98035e8b8b76bfe9f6cb4a5c926183ec5ff
Joe Danks <jdanks@Sources-MBP.localdomain>
2022-06-09 10:06:38 -0500

    Added jest and eslint, starting to move files around

---
+++ src/dynamodb.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk')
+const region = 'us-east-1'
+const tableName = 'small-improvements-goals'
+const dbClient = new AWS.DynamoDB({
+  apiVersion: '2012-08-10',
+  region
+})
+
+async function getRecord (key) {
+
+}
+
+async function insertRecord (activity) {
+
+}
+
+exports.getRecord = getRecord
+exports.insertRecord = insertRecord
--- src/index.js
+++ src/index.js
@@ -1,195 +1,135 @@
-'use strict';
+'use strict'
 
-const https = require('https');
 // Load the AWS SDK
-var AWS = require('aws-sdk'),
-    region = "us-east-1",
-    secretName = "arn:aws:secretsmanager:us-east-1:729161019481:secret:SIBot-Tokens-J4t6Af",/* API Tokens */
-    secret,
-    decodedBinarySecret;
+const AWS = require('aws-sdk')
+const secretsClient = require('./secrets')
+const smallImprovementsClient = require('./small-improvements')
+const dynamodbClient = require('./dynamodb')
+const region = 'us-east-1'
 // Create DynamoDB document client
 const docClient = new AWS.DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10',
-    region: region
-});
-// Create a Secrets Manager client
-var client = new AWS.SecretsManager({
-    region: region
-});
+  apiVersion: '2012-08-10',
+  region
+})
 
-const screeningHours = 24*30;
+const screeningHours = 24 * 30
 
-
-function getSecret(secretName){
-    return new Promise((accept,reject)=>{
-        client.getSecretValue({SecretId: secretName}, function(err, data) {
-            if(err){reject(`Could not get Secret: ${err.code}`);}
-            else{
-                // Decrypts secret using the associated KMS key.
-                // Depending on whether the secret is a string or binary, one of these fields will be populated.
-                if('SecretString' in data){accept(data.SecretString);}
-                else{
-                    let buff = new Buffer(data.SecretBinary, 'base64');
-                    accept(buff.toString('ascii'));
-                }
-            }
-        });
-    });
+const dynamoParams = {
+  TableName: 'small-improvements-goals' // As found in template.yaml
 }
 
-
-
-
-var httpsOptions = {
-    hostname: 'allies.small-improvements.com',
-    port: 443,
-    path: '/api/v2/activities?modules=OBJECTIVE',
-    method: 'GET',
-    headers: {
-        Accept: `application/json`,
-        'User-Agent': 'SIBot' /*IF YOU REMOVE THIS LINE I WILL BREAK*/
-    }
-};
-
-var dynamoParams = {
-    TableName : "small-improvements-goals" //As found in template.yaml
-}
-
-
-
-
-
-function getObjectives(opts){
-    return new Promise((accept,reject)=>{
-        const req = https.request(opts, res => {
-            var toReturn = "";
-            if(res.statusCode != 200){
-                console.log(`status logged ${res.statusCode}`);
-                reject(`Could not get objectives: ${res.statusCode}`);
-                return;
-            }
-            res.on('data', d => { //Concat new string onto old string
-                toReturn += d;
-            });
-            res.on('close',()=>{
-                accept(formatJSON(JSON.parse(toReturn)));
-            });
-        });
-        req.on('error', err => {
-            reject(`https error: ${err}`);
-        });
-        req.end();
-    });
-    
-}
-
-function formatJSON(json){
-    let now = new Date();
-    return json.items.flatMap(i => i.items)
+function formatJSON (json) {
+  const now = new Date()
+  return json.items.flatMap(i => i.items)
     .flatMap(i => i.activities)
-    .filter(a => a.type === "OBJECTIVE_STATUS_CHANGED")
+    .filter(a => a.type === 'OBJECTIVE_STATUS_CHANGED')
     .filter(a => a.change.newStatus.status === 100 || a.change.newStatus.status === 103)
-    .filter(a => a.content.objective.visibility === "PUBLIC")
-    .filter(a => a.occurredAt >= now-(screeningHours*3660*1000));
+    .filter(a => a.content.objective.visibility === 'PUBLIC')
+    .filter(a => a.occurredAt >= now - (screeningHours * 3660 * 1000))
 }
 
-async function getDatabase(){
-    //pull whole database?
+async function getDatabase () {
+  // pull whole database?
 }
 
-function postToSlack(posts){//posts are an array
-    for(var i=0;i<posts.length;i++){
-        //--------------------------------------------------
-    }
+function postToSlack (posts) { // posts are an array
+  for (let i = 0; i < posts.length; i++) {
+    // --------------------------------------------------
+  }
 }
 
-async function main(event,context,callback){
-    var secrets, SIToken, objectives, slackToken;
-    let rightNow = new Date(event.time);
-    let earliestTime = rightNow - (1000*60*12);
-    let tryDB = false;
-    try{
-        let secrets = await getSecret(secretName);
-        let SIToken = secrets.SIToken;
-        httpsOptions.headers.Authorization = `Bearer ${SIToken}`;
-        let slackToken = secrets.SlackToken;
-        let objectives = await getObjectives(httpsOptions);
-        objectives = objectives.items;
-        if(objectives.length>0){tryDB = true;}
-    }catch(err){console.log(err);}
-    if(tryDB){
-        //Get list of eligible ids
-        let ids = [];
-        for(var i=0;i<objectives.length;i++){
-            ids.push(objectives[i].content.objectives.id);
-        }
-        //Get All DB Entries
-        let dbEntries = await scanTable();
-        var dbIDs = dbEntries.map(entry=>entry.ID); //Is now just an array of ID's
-        let newEntries = [];
-        ids.forEach(entry=>{
-            if(!dbIDs.includes(entry)){
-                newEntries.push(entry);
-            }
-        });
-        //-----------------Push all updates to slack
-        //-----------------Loop through new entries, put them in DB
-        
-        
+async function main (event, context, callback) {
+  // let secrets, SIToken, objectives, slackToken
+  // const rightNow = new Date(event.time)
+  // const earliestTime = rightNow - (1000 * 60 * 12)
+  // let tryDB = false
+  // try {
+  //   const secrets = await secretsClient.getSecret()
+  //   const SIToken = secrets.SIToken
+  //   httpsOptions.headers.Authorization = `Bearer ${SIToken}`
+  //   const slackToken = secrets.SlackToken
+  //   let objectives = await smallImprovementsClient.getObjectives(SIToken)
+  //   objectives = objectives.items
+  //   if (objectives.length > 0) { tryDB = true }
+  // } catch (err) { console.log(err) }
+  // if (tryDB) {
+  //   // Get list of eligible ids
+  //   const ids = []
+  //   for (let i = 0; i < objectives.length; i++) {
+  //     ids.push(objectives[i].content.objectives.id)
+  //   }
+  //   // Get All DB Entries
+  //   const dbEntries = await scanTable()
+  //   const dbIDs = dbEntries.map(entry => entry.ID) // Is now just an array of ID's
+  //   const newEntries = []
+  //   ids.forEach(entry => {
+  //     if (!dbIDs.includes(entry)) {
+  //       newEntries.push(entry)
+  //     }
+  //   })
+  //   // -----------------Push all updates to slack
+  //   // -----------------Loop through new entries, put them in DB
+  // }
+  // // console.log('Received event:', JSON.stringify(event, null, 2));
+  // // callback(null, 'Finished');
 
-    }
-    //console.log('Received event:', JSON.stringify(event, null, 2));
-    //callback(null, 'Finished');
+  /*
+    Get secrets
+    Get SI objectives
+    Filter to recently completed objectives
+    For each recently completed objective
+      Query for record in dynamodb
+      No record
+        Post message to Slack
+        Put record in dynamodb
+  */
 }
 
-const dbQuery = async (pid)=>{
-    const paramss = {
-        TableName: dynamoParams.TableName,
-        region: 'us-east-1'
-    };
+const dbQuery = async (pid) => {
+  const paramss = {
+    TableName: dynamoParams.TableName,
+    region: 'us-east-1'
+  }
 
-    let toOut = await docClient.query(paramss).promise();
-    return toOut;
-};
+  const toOut = await docClient.query(paramss).promise()
+  return toOut
+}
 
-const putItem = async (pid)=>{
-    const paramss = {
-        TableName: dynamoParams.TableName,
-        region: 'us-east-1'
-    };
-};
+const putItem = async (pid) => {
+  const paramss = {
+    TableName: dynamoParams.TableName,
+    region: 'us-east-1'
+  }
+}
 
 const scanTable = async () => {
-    const paramss = {
-        TableName: dynamoParams.TableName,
-        region: 'us-east-1'
-    };
-    const scanResults = [];
-    var itemss;
-    do{
-        itemss =  await docClient.scan(paramss).promise();
-        itemss.Items.forEach((item) => scanResults.push(item));
-        paramss.ExclusiveStartKey  = itemss.LastEvaluatedKey;
-    }while(typeof itemss.LastEvaluatedKey !== "undefined");
-    return scanResults;
-};
+  const paramss = {
+    TableName: dynamoParams.TableName,
+    region: 'us-east-1'
+  }
+  const scanResults = []
+  let itemss
+  do {
+    itemss = await docClient.scan(paramss).promise()
+    itemss.Items.forEach((item) => scanResults.push(item))
+    paramss.ExclusiveStartKey = itemss.LastEvaluatedKey
+  } while (typeof itemss.LastEvaluatedKey !== 'undefined')
+  return scanResults
+}
 
-exports.handler = main;
-exports.main = main;
-exports.getObjectives = getObjectives;
+exports.handler = main
+exports.main = main
+exports.getObjectives = getObjectives
 
 // schedule with cloudwatch rule -> cron(0 */12 * * *);
 
-//For DynamoDB, reference the following:
+// For DynamoDB, reference the following:
 // https://docs.amplify.aws/guides/functions/dynamodb-from-js-lambda/q/platform/js/#scanning-a-table
-
 
 // Use this code snippet in your app.
 // If you need more information about configurations or implementing the sample code, visit the AWS docs:
 // https://aws.amazon.com/developers/getting-started/nodejs/
 
-
 // In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
 // See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
 // We rethrow the exception by default.
-
