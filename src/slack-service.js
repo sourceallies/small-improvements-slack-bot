70e4e0bc6441933dcc9da44b5d8fe0b1da906c37
Neil Bode <nbode@sourceallies.com>
2025-10-29 16:35:55 -0500

    revert-slack-channel-id (#46)
    * Revert "Fix default (aka dev)"
    
    This reverts commit 5b16e3a37b8196c7406d43118bc2178a46ebda88.
    
    * Revert "Fix config in prod!"
    
    This reverts commit 81b0cebf2a54becdb7002b0a30433bffa949027c.
    
    * Revert "Update Slack integration to use `channelId` instead of `channelName`"
    
    This reverts commit 42578e4c95df87f35f77c642ceb297b03de682f5.

--- src/slack-service.js
+++ src/slack-service.js
@@ -1,15 +1,15 @@
 const slackClient = require('./slack');
 
 // Get Slack ID, Format Message, Post
-async function PostCompletedObjective(token, channelId, content, newStatus, email) {
+async function PostCompletedObjective(token, channelName, content, newStatus, email) {
   const slackID = await slackClient.getSlackID(email, token);
   const formattedMessage = await slackClient.formatSlackMessageForCompleted(content.objective, newStatus, slackID, content.cycle.id);
-  return await slackClient.slackPost(token, channelId, formattedMessage);
+  return await slackClient.slackPost(token, channelName, formattedMessage);
 }
-async function PostCreatedObjective(token, channelId, content, email) {
+async function PostCreatedObjective(token, channelName, content, email) {
   const slackID = await slackClient.getSlackID(email, token);
   const formattedMessage = await slackClient.formatSlackMessageForCreated(content.objective, slackID, content.cycle.id);
-  return await slackClient.slackPost(token, channelId, formattedMessage);
+  return await slackClient.slackPost(token, channelName, formattedMessage);
 }
 
 exports.PostCompletedObjective = PostCompletedObjective;
